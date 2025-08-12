616edb185e641a8ba14edad95666f08954e35d46
vem4s <vem4s@users.noreply.github.com>
2025-08-12 00:11:20 +0000

    deploy: 3b1931d60e553d2b531c9587328d4881b694a4a2

--- build.mjs
+++ build.mjs
@@ -44,37 +44,11 @@ let htmlBody = marked.parse(markdown);
 // Post-process gallery wrapping
 htmlBody = htmlBody.replace(
   /<h2>Screenshots<\/h2>\s*<p>(.*?)<\/p>/s,
-  (_, imgs) => `<h2>Screenshots</h2>\n<div class="gallery">${imgs}</div>`
+  (_, imgs) => `<h2>Screenshots</h2>\n<div class=\"gallery\">${imgs}<\/div>`
 );
 
 // Wrap in your index.html structure
-const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-	<link rel="preconnect" href="https://api.fonts.coollabs.io" crossorigin />
-	<link href="https://api.fonts.coollabs.io/css2?family=Cascadia+Mono:ital@0;1&display=swap" rel="stylesheet">
-	<link rel="icon" type="image/x-icon" href="favicon.ico"/>
-	
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta content="Twinkie" property="og:title" />
-	<meta content="Twinkie is a free and open source QOL modification for Trackmania Forever." property="og:description" />
-	<meta content="https://github.com/TwinkieTweaks/Twinkie/blob/master/Media/t528.png?raw=true" property="og:image" />
-	<meta content="#ff00ff" data-react-helmet="true" name="theme-color" />
-	
-	<title>Twinkie - Trackmania Forever</title>
-	<link rel="stylesheet" href="styles.css">
-</head>
-<body>
-	<center>
-		<div id="main-container" class="cascadia-mono">
-			<div id="secondary-container">
-${htmlBody.trim()}
-			</div>
-		</div>
-	</center>
-</body>
-</html>`;
+const html = `<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n\t<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n\t<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n\t<link href=\"https://fonts.googleapis.com/css2?family=Cascadia+Mono:ital@0;1&display=swap\" rel=\"stylesheet\"> \n\t<link rel=\"icon\" type=\"image/x-icon\" href=\"favicon.ico\"/>\n\t\n\t<meta charset=\"UTF-8\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<meta content=\"Twinkie\" property=\"og:title\" />\n\t<meta content=\"Twinkie is a free and open source QOL modification for Trackmania Forever.\" property=\"og:description\" />\n\t<meta content=\"https://github.com/TwinkieTweaks/Twinkie/blob/master/Media/t528.png?raw=true\" property=\"og:image\" />\n\t<meta content=\"#ff00ff\" data-react-helmet=\"true\" name=\"theme-color\" />\n\t\n\t<title>Twinkie - Trackmania Forever</title>\n\t<link rel=\"stylesheet\" href=\"styles.css\">\n</head>\n<body>\n\t<center>\n\t	<div id=\"main-container\" class=\"cascadia-mono\">\n\t		<div id=\"secondary-container\">\n${htmlBody.trim()}\n\t		</div>\n\t	</div>\n\t</center>\n</body>\n</html>`;
 
 fs.writeFileSync("index.html", html);
 console.log("✅ index.html generated successfully.");
